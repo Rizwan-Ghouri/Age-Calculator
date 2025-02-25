e53d63074c1bcefe330ed3b602fe307fda6bd55c
Rizwan-Ghouri <rizwanidrees138@gmail.com>
2025-02-25 23:58:34 +0500

    adding functionalty

--- app.js
+++ app.js
@@ -10,25 +10,34 @@ get_Age.addEventListener("click", () => {
     if (inpCur != "" && inpbir != "") {
         const startYear = new Date(`${inpbir}`).getFullYear();
         const endYear = new Date(`${inpCur}`).getFullYear();
-        let minusvalue = endYear - startYear;        
-    
-        const millisecondsDifference = (endYear - startYear) / (1000 * 60 * 60 * 24);
-        show.innerText = `Age ${minusvalue} , ${millisecondsDifference}`
+        const startDate = new Date(`${inpbir}`).getTime();
+        const endDate = new Date(`${inpCur}`).getTime();
+        let minusYear = endYear - startYear;
+        let minusvalue = endDate - startDate;
+
+        const millisecondsDifference = minusvalue / (1000 * 60 * 60 * 24);
+        const diffMilliseconds = minusvalue;
+        const diffSeconds = diffMilliseconds / 1000;
+        const diffMinutes = diffSeconds / 60;
+        const diffHours = diffMinutes / 60;
+        const diffDays = diffHours / 24;
+
+        console.log(`Milliseconds: ${diffMilliseconds}`);
+        console.log(`Seconds: ${diffSeconds}`);
+        console.log(`Minutes: ${diffMinutes}`);
+        console.log(`Hours: ${diffHours}`);
+        console.log(`Days: ${diffDays}`);
+        show.innerHTML = `
+                    <div class="dataShow">Age <br> ${minusYear}</div>
+                    <div class="dataShow">Days <br> ${diffDays}</div>
+                    <div class="dataShow">Hours <br> ${diffHours}</div>
+                    <div class="dataShow">Minutes <br> ${diffMinutes}</div>
+                    <div class="dataShow">Seconds <br> ${diffSeconds}</div>
+                    <div class="dataShow">Milliseconds <br> ${diffMilliseconds}</div>
+        `
     } else {
-        show.innerText = "please inpur filled"
+        show.innerText = "please input filled"
     }
 })
-const startDate = new Date("2000-01-01");
-const endDate = new Date("2025-01-01");
 
-const diffMilliseconds = endDate - startDate;
-const diffSeconds = diffMilliseconds / 1000;
-const diffMinutes = diffSeconds / 60;
-const diffHours = diffMinutes / 60;
-const diffDays = diffHours / 24;
 
-console.log(`Milliseconds: ${diffMilliseconds}`);
-console.log(`Seconds: ${diffSeconds}`);
-console.log(`Minutes: ${diffMinutes}`);
-console.log(`Hours: ${diffHours}`);
-console.log(`Days: ${diffDays}`);
